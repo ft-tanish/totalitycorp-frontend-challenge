bb3507030862f52395464c03815976640e28d514
Tanish <tanishchaurasia6@gmail.com>
2022-10-19 03:38:59 +0530

    first commit

---
+++ src/components/Login/Login.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Login = (props) => {
+    return (
+        <Cointainer>
+            <Nav>
+                <a href="/"></a>
+                <img src="/image/login-logo.svg" alt="" />
+            </Nav>
+        </Cointainer>
+    )
+}
+
+const Cointainer = styled.div``;
+
+const Nav = styled.nav``;
+
+export default Login
