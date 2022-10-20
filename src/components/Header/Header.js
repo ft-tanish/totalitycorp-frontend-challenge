1fe09a15b4496f66a3680a3e7d1aa748da43fb3d
Tanish <tanishchaurasia6@gmail.com>
2022-10-21 01:24:27 +0530

    nav-done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,6 +7,10 @@ import Network from '../../assests/nav-network.svg'
 import Jobs from '../../assests/nav-jobs.svg'
 import Message from '../../assests/nav-messaging.svg'
 import Notifications from '../../assests/nav-notifications.svg'
+import User from '../../assests/user.svg'
+import DownIcon from '../../assests/down-icon.svg'
+import Work from '../../assests/nav-work.svg'
+
 
 
 const Header = () => {
@@ -30,34 +34,60 @@ const Header = () => {
                 <div className='headnav'>
                     <div className='navlistwrap'>
                         <div className='navlist'>
-                            <a>
+                            <a href="/home">
                                 <img src={NavHomeIcon} alt="" />
                                 <span>Home</span>
                             </a>
                         </div>
                         <div className='navlist'>
-                            <a>
+                            <a href=" ">
                                 <img src={Network} alt="" />
                                 <span>My Network</span>
                             </a>
                         </div>
                         <div className='navlist'>
-                            <a>
+                            <a href=" ">
                                 <img src={Jobs} alt="" />
                                 <span>Jobs</span>
                             </a>
                         </div>
                         <div className='navlist'>
-                            <a>
+                            <a href=" ">
                                 <img src={Message} alt="" />
                                 <span>Messaging</span>
                             </a>
                         </div>
                         <div className='navlist'>
-                            <a>
+                            <a href=" ">
                                 <img src={Notifications} alt="" />
                                 <span>Notifications</span>
                             </a>
+                        </div>
+                        <div className='navlist'>
+                            <div className='user'>
+                                <a href=" ">
+                                    <img src={User} alt='' />
+                                    <span>Me</span>
+                                    <img src={DownIcon} alt="" />
+                                </a>
+                                <div className='signout'>
+                                    <a href=" ">
+                                        Sign Out
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='navlist'>
+                            <div className='user'>
+                                <div className='work'>
+                                    <a href=" ">
+                                        <img src={Work} alt='' />
+                                        <span>Work
+                                            <img src={DownIcon} alt="" />
+                                        </span>
+                                    </a>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
