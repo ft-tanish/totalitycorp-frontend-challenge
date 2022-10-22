07993b9b1fd75ac1d1ee9c30b4ada40902737635
Tanish <tanishchaurasia6@gmail.com>
2022-10-22 19:59:19 +0530

    right done

--- src/components/Right/Right.js
+++ src/components/Right/Right.js
@@ -1,8 +1,52 @@
 import React from 'react'
+import './Right.css'
+import FeedIcon from '../../assests/feed-icon.svg'
+import RightIcon from '../../assests/right-icon.svg'
 
 const Right = () => {
     return (
-        <div>Right</div>
+        <div className="containerRight">
+            <div className='followCard'>
+                <div className='title'>
+                    <h2>Add to your feed</h2>
+                    <img src={FeedIcon} alt="feed" />
+                </div>
+
+                <div className='feedList'>
+                    <li>
+                        <a href=" ">
+                            <div className='avtar'></div>
+                        </a>
+                        <div>
+                            <span>#Linkedin</span>
+                            <button>Follow</button>
+                        </div>
+                    </li>
+                    <li>
+                        <a href=" ">
+                            <div className='avtar'></div>
+                        </a>
+                        <div>
+                            <span>#Video</span>
+                            <button>Follow</button>
+                        </div>
+                    </li>
+                </div>
+
+                <div className='recommendation'>
+                    View all recommendations
+                    <img src={RightIcon} alt="view all" />
+                </div >
+            </div>
+            <div className='followCard'>
+                <div className='bannerCard'>
+                    <img
+                        src="https://static-exp1.licdn.com/scds/common/u/images/promo/ads/li_evergreen_jobs_ad_300x250_v1.jpg"
+                        alt="linked"
+                    />
+                </div>
+            </div>
+        </div>
     )
 }
 
